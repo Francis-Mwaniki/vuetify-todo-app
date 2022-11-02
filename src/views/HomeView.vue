b6f1276c539fa2be852071651bae5901f04c9595
Francis-Mwaniki <francismwaniki630@gmail.com>
2022-11-02 20:00:29 +0300

    vuetif-todo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,39 @@
 <template>
-  <div class="" >
+  <div class="">
+    <v-text-field
+      outlined
+      label="Add todo.."
+      append-icon="mdi-plus"
+      hide-details
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      clearable
+      class="pa-3"
+      v-model="newText"
+    ></v-text-field>
     <v-list flat>
-      <v-list-item @click="doneTask(task.id)" :class="{'blue accent-1' : task.done}" v-for="task in tasks" :key="task.id">
+      <v-list-item
+        @click="doneTask(task.id)"
+        :class="{ 'blue accent-1': task.done }"
+        v-for="task in tasks"
+        :key="task.id"
+      >
         <template v-slot:default>
           <v-list-item-action>
             <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
           </v-list-item-action>
 
           <v-list-item-content>
-            <v-list-item-title :class="{'text-decoration-line-through' : task.done}"  >{{ task.title }}</v-list-item-title>
+            <v-list-item-title
+              :class="{ 'text-decoration-line-through': task.done }"
+              >{{ task.title }}</v-list-item-title
+            >
           </v-list-item-content>
+          <v-list-item-action>
+            <v-icon color="primary" @click.stop="deleteTask(task.id)"
+              >mdi-delete-circle</v-icon
+            >
+          </v-list-item-action>
         </template>
       </v-list-item>
     </v-list>
@@ -25,6 +49,7 @@ export default {
   components: {},
   data() {
     return {
+      newText: "",
       settings: [],
       tasks: [
         { id: 1, done: false, title: "wakeup" },
@@ -34,9 +59,20 @@ export default {
     };
   },
   methods: {
+    addTask() {
+      let newTask = {
+        id: Date.now(),
+        title: this.newText,
+        done: false,
+      };
+      this.tasks.push(newTask);
+    },
     doneTask(id) {
       let task = this.tasks.filter((task) => task.id === id);
-       task[0].done=!task[0].done
+      task[0].done = !task[0].done;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
     },
   },
 };
